8ef4f9bc00c1c983d0b4dd3da37e1e8fbbe25fe3
TaeYunAhn <zzz6874@naver.com>
2024-12-26 23:20:31 +0900

    Updates

---
+++ precache-manifest.2f8a774ae2b8d6b1da41667d86331e8c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75ac4ca80f3a8990af1ffb29ac284cbe",
+    "url": "/JBNU_wsd_kakao/index.html"
+  },
+  {
+    "revision": "c645ded41f691a3d9988",
+    "url": "/JBNU_wsd_kakao/static/css/main.6822e550.chunk.css"
+  },
+  {
+    "revision": "27ac4aea6c416f511661",
+    "url": "/JBNU_wsd_kakao/static/js/2.1fc5e40f.chunk.js"
+  },
+  {
+    "revision": "2da257790f1aed96437033b440d07378",
+    "url": "/JBNU_wsd_kakao/static/js/2.1fc5e40f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c645ded41f691a3d9988",
+    "url": "/JBNU_wsd_kakao/static/js/main.21d1e199.chunk.js"
+  },
+  {
+    "revision": "5dcca24c55148eb60044",
+    "url": "/JBNU_wsd_kakao/static/js/runtime-main.25851c8d.js"
+  },
+  {
+    "revision": "d8c456cc4698f8e3218b8c505e9d9db4",
+    "url": "/JBNU_wsd_kakao/static/media/terms.d8c456cc.md"
+  }
+]);
